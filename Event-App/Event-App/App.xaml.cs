d17005d090b2c91b948f558395d85a06ae0aed39
Mike Overbeek <mikeoverbeek@outlook.com>
2020-04-09 16:04:30 +0200

    Add project files.

---
+++ Event-App/Event-App/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Event_App.Services;
+using Event_App.Views;
+
+namespace Event_App
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
