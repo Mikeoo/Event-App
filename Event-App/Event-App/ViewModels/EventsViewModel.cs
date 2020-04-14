26ae2ef0a82ff48b6b9892ecdd7c762af78da1b7
Mike Overbeek <mikeoverbeek@outlook.com>
2020-04-14 12:15:30 +0200

    link working to events page

---
+++ Event-App/Event-App/ViewModels/EventsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Event_App.ViewModels
+{
+    public class EventsViewModel : BaseViewModel
+    {
+        public EventsViewModel()
+        {
+            Title = "Events";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://www.driessengroep.nl/"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
